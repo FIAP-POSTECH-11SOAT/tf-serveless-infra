0b029f234194b3271c74b8bad26fdf599ac33348
Wellison Pereira <wellison28@gmail.com>
2025-10-07 19:29:48 -0300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -11,7 +11,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "rmsterraformstatefiap"
+    bucket  = "rmsterraformstate-0610"
     key     = "infra/prod/serverless/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
